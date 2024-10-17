8f43775a9aae2e367d9894c29618e5c4dd31fbda
Keisuke Sehara <keisuke.sehara@gmail.com>
2024-10-17 20:33:41 +0900

    fix: load_downsampled_trials: transpose trials

--- bdbc_nwb_packager/packaging/trials.py
+++ bdbc_nwb_packager/packaging/trials.py
@@ -87,47 +87,16 @@ def load_trials(
 
 
 def load_downsampled_trials(
-    trials: _pd.DataFrame,
-    timebases: _core.Timebases,
-    tasktype: str = 'cued_lever_pull',
-    verbose: bool = True,
-) -> _TimeIntervals:
-    task = getattr(_trials, tasktype)
-    trials_ds = _alignment.align_trials_to_pulses(
-        trials,
-        timebases.raw,
-        timebases.dFF,
-        columnsettings=task.COLUMN_TYPES,
-    )
-    trials_ds['start_time'] = timebases.dFF[trials_ds.start.values]
-    trials_ds['stop_time']  = timebases.dFF[trials_ds.end.values]
-
-    for col, typ in task.COLUMN_TYPES.items():
-        if col in ('start', 'stop'):
-            continue
-        elif typ == 'time':
-            trials_ds[f"{col}_time"] = index_to_timestamp(
-                trials_ds[col].values,
-                timebases.dFF,
-            )
-    # rawdataから読み込んでdtaframede返すなコードを書く
-    return trials_ds # dataframe
-
-def _load_downsampled_trials(
     rawfile: PathLike,
 ) -> _pd.DataFrame:
     with _h5.File(rawfile, 'r') as src:
         # Loading from hdf5 file
-        data        = _np.array(src["behavior_ds/trial_info/data"], dtype=_np.uint32)
+        data        = _np.array(src["behavior_ds/trial_info/data"], dtype=_np.float32).T
         label       = _np.array(src["behavior_ds/trial_info/label"]).ravel()
         label       = [lab.decode('utf-8') for lab in label] # convert to utf-8
-        
         # convert to dataframe
-        trials_ds = _pd.DataFrame(data, index=label)
-        trials_ds.columns = [i for i in range(1, 132)]
-        
+        trials_ds = _pd.DataFrame(data, columns=label)
         return trials_ds
-    
 
 
 def format_trials(
